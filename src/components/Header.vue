3cbe41090093d82618cebc6eae27cdebf349d9c4
Tosin Ayodele <devtosxn@gmail.com>
2021-09-15 22:36:20 +0100

    add Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,10 @@
 <template>
-  Header
+<header class="text-center bg-blue-800 text-white p-4 mb-10">
+  <div class="text-3xl md:text-5xl font-bold mb-3">
+   <i class="fa fa-virus"></i> Covid-19 Tracker
+  </div>
+  <p>API developed by <a class="text-blue-300" href="https://covid19api.com" target="_blank">covid19api.com</a></p>
+</header>
 </template>
 
 <script>
