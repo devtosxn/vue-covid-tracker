f2dc83b6dcb825c3773d93216cb60961f0efbea7
Tosin Ayodele <devtosxn@gmail.com>
2021-09-15 23:01:06 +0100

    setup network request from API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,46 @@
 <template>
-  <div class="home">
-    Hello World!
-  </div>
+  <main v-if="!loading">
+API Data Loaded
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center"
+  v-else>
+<div class="text-gray-500 text-3xl mt-10 mb-6">
+  Fetching Data
+</div>
+<img :src="loadingImage" class="w-24 m-auto" alt="loading">
+  </main>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data () {
+    return{
+      loading: true,
+      title: 'Global',
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+    },
   components: {},
+  methods: {
+   async fetchCovidData () {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+      }
+    },
+  async created() {
+    console.log('Home created');
+    const data = await this.fetchCovidData();
+    console.log(data);
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+  },
 }
 </script>
