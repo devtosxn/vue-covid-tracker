327c7e7af7d97779dca2b20468fd04862b02e3c8
Tosin Ayodele <devtosxn@gmail.com>
2021-09-17 12:43:17 +0100

    add functionality to get data for each country

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <main v-if="!loading">
       <DataTitle :text="title" :dataDate="dataDate" />
       <DataBoxes :stats="stats"/>
+      <CountrySelect @get-country="getCountryData" :countries="countries" />
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -15,12 +16,14 @@
 <script>
 import DataTitle from '@/components/DataTitle'
 import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 
 export default {
   name: 'Home',
   components: {
     DataTitle,
-    DataBoxes
+    DataBoxes,
+    CountrySelect
     },
   data () {
     return{
@@ -37,7 +40,11 @@ export default {
       const res = await fetch('https://api.covid19api.com/summary')
       const data = await res.json()
       return data
-      }
+      },
+    getCountryData (country) {
+      this.stats = country
+      this.title = country.Country
+    } 
     },
   async created() {
     console.log('Home created');
